fcff117a146ca025c141ac10e71b61e426ac1163
Lordsisodia <130863457+Lordsisodia@users.noreply.github.com>
2024-06-14 09:30:08 +0100

    Fix ESLint issues in build script and ensure Node version compatibility

--- utils/build.js
+++ utils/build.js
@@ -3,10 +3,16 @@ process.env.NODE_ENV = 'production';
 process.env.ASSET_PATH = '/';
 
 try {
+  /* eslint-disable no-console */
   console.log('Attempting to require webpack...');
-  const webpack = require('webpack');
-  console.log('Webpack successfully required.');
+  // Disable eslint rule for global require
+  // eslint-disable-next-line global-require
+  const webpack = require('../node_modules/webpack');
+  console.log('Webpack successfully required:', webpack.version);
+  // eslint-disable-next-line global-require
   const config = require('../webpack.config');
+  console.log('Webpack config loaded:', JSON.stringify(config, null, 2));
+  /* eslint-enable no-console */
 
   delete config.chromeExtensionBoilerplate;
 
@@ -14,12 +20,18 @@ try {
 
   webpack(config, function (err, stats) {
     if (err) {
+      /* eslint-disable no-console */
       console.error('Webpack build error:', err);
+      /* eslint-enable no-console */
       throw err;
     }
+    /* eslint-disable no-console */
     console.log(stats.toString({ colors: true }));
+    /* eslint-enable no-console */
   });
 } catch (error) {
+  /* eslint-disable no-console */
   console.error('Build script error:', error);
+  /* eslint-enable no-console */
   process.exit(1);
 }
