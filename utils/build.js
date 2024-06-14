054d2028ffff68365be891c99df47af9b8d5ee25
Lordsisodia <130863457+Lordsisodia@users.noreply.github.com>
2024-06-14 09:35:45 +0100

    Disable ESLint rules for dynamic require in build script

--- utils/build.js
+++ utils/build.js
@@ -5,13 +5,14 @@ process.env.ASSET_PATH = '/';
 try {
   /* eslint-disable no-console */
   console.log('Attempting to require webpack...');
-  // Disable eslint rule for global require
-  // eslint-disable-next-line global-require
-  const webpack = require('../node_modules/webpack');
+  // Resolve the path to webpack
+  /* eslint-disable import/no-dynamic-require, global-require */
+  const webpackPath = require.resolve('webpack');
+  const webpack = require(webpackPath);
   console.log('Webpack successfully required:', webpack.version);
-  // eslint-disable-next-line global-require
   const config = require('../webpack.config');
   console.log('Webpack config loaded:', JSON.stringify(config, null, 2));
+  /* eslint-enable import/no-dynamic-require, global-require */
   /* eslint-enable no-console */
 
   delete config.chromeExtensionBoilerplate;
